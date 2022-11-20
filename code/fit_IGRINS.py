9474f50b8e0af3d349a081154e8b80d131bc6b03
Xueqing Chen <levinedeche@hotmail.com>
2022-11-20 15:42:46 +0000

    fit nonstack

--- code/fit_IGRINS.py
+++ code/fit_IGRINS.py
@@ -13,7 +13,8 @@ homedir = os.path.expanduser('~')
 
 def fit_stacked(target, modelpath, band):
     """
-    Fit averaged observations, wavelengths also averaged (One fit per order).
+    Fit averaged observations with the same guess for wcoef (fitted using wl of first/avg order) 
+    for all observations, i.e., one fit per order.
     """
     ###################################
     ##  Open IGRINS data
@@ -171,7 +172,9 @@ def fit_stacked(target, modelpath, band):
 
 def fit_ownwcoef(target, modelpath, band):
     """
-    Fit averaged observations, but each obs with own wcoef.
+    Fit averaged observations, but use wcoef guess fitted from each observations, 
+    thus Nobs fits per order. Within each order, the difference of those Nobs fits 
+    only come from the different wcoef guess used.
     """
 
     if target == "W1049B":
@@ -332,33 +335,45 @@ def fit_ownwcoef(target, modelpath, band):
     fits.writeto(f'{resultdir}/IGRINS_W1049B_{band}_chipmods_{modelname[:12]}.fits', chipmods, overwrite=True)
     fits.writeto(f'{resultdir}/IGRINS_W1049B_{band}_chiplams_{modelname[:12]}.fits', chiplams, overwrite=True)
 
-def fit_nonstack(modelpath):
+def fit_nonstack(target, modelpath, band):
     """
-    Fit non-averaged observations, each with specific wcoef.
+    Fit each observation separately, each with wcoef guess that come from their own wl.
     """
     ###################################
-    #  Open data
+    #  Open IGRINS data
     ###################################
+    if target == "W1049B":
+        filelist = sorted(glob.glob(f'{homedir}/uoedrive/data/IGRINS/SDCK*_1f.spec.fits'))
+        fluxes = []
+        wls = []
+        for filename in filelist:
+            wlname = filename.split('_1f')[0]+'.wave.fits'
 
-    filelist = sorted(glob.glob(f'{homedir}/uoedrive/data/IGRINS/SDCK*_1f.spec.fits'))
+            flux = fits.getdata(filename)
+            wl = fits.getdata(wlname)
 
-    fluxes = []
-    wls = []
+            # trim first and last 100 columns
+            flux = flux[:, 100:1948]
+            wl = wl[:, 100:1948]
+            
+            fluxes.append(flux)
+            wls.append(wl) 
 
-    for filename in filelist:
-        wlname = filename.split('_1f')[0]+'.wave.fits'
+    if target == "2M0036":
+        filelist = sorted(glob.glob(f'{homedir}/uoedrive/data/IGRINS_{target}/SDC{band}*.spec_a0v.fits'))
+        fluxes = []
+        wls = []
+        for filename in filelist:
+            hdu = fits.open(filename)
+            flux = hdu[0].data
+            wl = hdu[1].data
 
-        flux = fits.getdata(filename)
-        wl = fits.getdata(wlname)
+            # trim first and last 100 columns
+            flux = flux[:, 100:1948]
+            wl = wl[:, 100:1948]
 
-        # trim first and last 100 columns
-        flux = flux[:, 100:1948]
-        wl = wl[:, 100:1948]
-
-        #print(wl.shape)
-        
-        fluxes.append(flux)
-        wls.append(wl)
+            fluxes.append(flux)
+            wls.append(wl)
 
     ###############################
     ## Collapse data over the whole observation into one mean, smoothed spectrum
@@ -382,15 +397,22 @@ def fit_nonstack(modelpath):
     wls = wls[:, 1:24, :] 
 
     ##########################
-    # open model
+    # Open model
     ##########################
 
-    model = Table.read(modelpath, format='fits')
-    modelname = modelpath.split("/")[-1]
-    model['wl'] = model['Wavelength']
-    model['flux'] = model['Flux']
+    if "BTSettl" in modelpath:
+        model = Table.read(modelpath, format='fits')
+        modelname = modelpath.split("/")[-1]
+        model['wl'] = model['Wavelength']
+        model['flux'] = model['Flux']
 
-    # set # of observations and # of orders to process
+    if "Callie" in modelpath:
+        model = fits.getdata(modelpath)
+        modelname = modelpath.split("/")[-1]
+
+    ##########################
+    ## Fitting model
+    ##########################
     nobs = wls.shape[0]
     norders = 20  # skip last few non-fitting orders for now
     npix = dims[2] 
@@ -402,8 +424,6 @@ def fit_nonstack(modelpath):
     chiplams = np.zeros((nobs, norders, npix), dtype=float)
     chipmodnobroad = np.zeros((nobs, norders, npix), dtype=float)
     chipguesses = np.zeros((nobs, norders, npix), dtype=float)
-
-    # loop over each chip and fit the model to that chip
     chisqarr = np.zeros((nobs, norders), dtype=float)
 
     # set up tables for best fit vsini, limb darkening values, and rv
@@ -412,9 +432,12 @@ def fit_nonstack(modelpath):
     vsini = []
     lld = []
     rv = []
+    wcoefs = []
+    ccoefs = []
     chisq = []
 
     for jj in (np.arange(norders)):
+        print(f"Current fitting: model {modelname}, order {jj}.")
         lolim = wls[:, jj, :].min() - 0.003
         hilim = wls[:, jj, :].max() + 0.003
         tind = (model['wl']>lolim) * (model['wl'] < hilim)
@@ -422,7 +445,12 @@ def fit_nonstack(modelpath):
         template = model['flux'][tind]
         template /= np.median(template)
         
-        print(f"Current fitting: model {modelname}, order {jj}.")
+        # to be compatible with rotational profile convolution kernel
+        if len(lam_template) < 400:
+           new_lam = np.linspace(lam_template[0], lam_template[-1], 400)
+           template = np.interp(new_lam, lam_template, template)
+           lam_template = new_lam
+
         for obs in np.arange(nobs):
             wcoef = np.polyfit(pix, wls[obs, jj, :], NPW-1)
             # fit continuum coefficients / flux scaling
@@ -432,17 +460,11 @@ def fit_nonstack(modelpath):
             ccoef = np.polyfit(pix[fobs0[obs,jj]>ind90], fobs0[obs,jj][fobs0[obs,jj]>ind90], NPC-1)
 
             guess = np.concatenate(([21, 0.3, 9e-5], wcoef, ccoef))
-            #mygmod, mygw = fit_atmo.modelspec_tel_template(guess, lam_template, template, lam_atmo, atmo, NPW, NPC, npix, retlam=True)
-
-            thingy = mf.modelspec_template(guess, lam_template, template, NPW, NPC, npix)
-            chipguesses[obs,jj] = thingy
-            #sys.exit()
             fitargs = (mf.modelspec_template, lam_template, template, NPW, NPC, npix, fobs0[obs,jj], wfobs0[obs,jj])
-
             fit = mf.fmin(mf.errfunc, guess, args=fitargs, full_output=True, disp=True, maxiter=10000, maxfun=100000)
-            print(fit[0][0:4])
+            print("fitted params:", fit[0])
+            print("chisq:", fit[1])
             mymod, myw = mf.modelspec_template(fit[0], *fitargs[1:-2], retlam=True)
-            #mycor = mf.modelspec_tel_template(fit[0], lam_template, np.ones(template.size), *fitargs[3:-2], retlam=False)
             chipfits.append(fit)
             chipmods[obs,jj] = mymod
             chiplams[obs,jj] = myw
@@ -453,9 +475,15 @@ def fit_nonstack(modelpath):
             vsini.append(fit[0][0])
             lld.append(fit[0][1])
             rv.append(fit[0][2])
+            wcoefs.append(fit[0][3:7])
+            ccoefs.append(fit[0][7:])
             chisq.append(fit[1])
 
             chisqarr[obs,jj] = fit[1]
+
+    ##########################
+    ## Save result
+    ##########################
 
     # make table of best parameters
     results = Table()
@@ -465,11 +493,17 @@ def fit_nonstack(modelpath):
     results['vsini'] = vsini
     results['lld'] = lld
     results['rv'] = rv
+    results['wcoef'] = [f"{wcoef[0]}, {wcoef[1]}, {wcoef[2]}, {wcoef[3]}" for wcoef in wcoefs]
+    results['ccoef'] = [f"{ccoef[0]}, {ccoef[1]}" for ccoef in ccoefs]
+
 
     if "BTSettl" in modelpath:
-        results.write(f'result/IGRINS_W1049B_K_nonstack_fitting_results_{modelname[:12]}.txt', format='ascii')
-        fits.writeto(f'result/IGRINS_W1049B_K_nonstack_chipmods_{modelname[:12]}.fits', chipmods)
-        fits.writeto(f'result/IGRINS_W1049B_K_nonstack_chiplams_{modelname[:12]}.fits', chiplams)
+        resultdir = f"{homedir}/uoedrive/result/CIFIST"
+    if "Callie" in modelpath:
+        resultdir = f"{homedir}/uoedrive/result/Callie"
+    results.write(f'{resultdir}/IGRINS_{target}_{band}_nonstack_fitting_results_{modelname[:12]}.txt', format='ascii', overwrite=True)
+    fits.writeto(f'{resultdir}/IGRINS_{target}_{band}_nonstack_chipmods_{modelname[:12]}.fits', chipmods, overwrite=True)
+    fits.writeto(f'{resultdir}/IGRINS_{target}_{band}_nonstack_chiplams_{modelname[:12]}.fits', chiplams, overwrite=True)
 
 
 if __name__ == "__main__":
@@ -501,4 +535,4 @@ if __name__ == "__main__":
     modellist = sorted(glob.glob(f"{homedir}/uoedrive/data/{modeldir}/*.fits"))
     for model in modellist:
         print(f"***Running fit to model {model}***")
-        fit_stacked(target=target, modelpath=model, band=band)
+        fit_nonstack(target=target, modelpath=model, band=band)
